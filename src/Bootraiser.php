36cbf1e7e9436289a878d545d4a53cc1e1b733d2
RT Filefabrik <rt@filefabriik.com>
2024-05-19 17:42:53 +0200

    init

---
+++ src/Bootraiser.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * PHP version 8.2
+ *
+ */
+/** @copyright-header * */
+
+namespace Filefabrik\Booteraise;
+/**
+ * insert it into your YourPackageServiceProvider
+ */
+trait Bootraiser
+{
+
+}
